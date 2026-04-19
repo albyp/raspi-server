f6d7cc5aaebcb641a7c2d9627a94bf884c264a5e
albyp <alby.palmer@hotmail.com>
2026-04-19 17:11:55 +0800

    Update env to pull from root or server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import statusRoute from './routes/status.js';
 
-dotenv.config();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: path.join(__dirname, '../.env') });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
